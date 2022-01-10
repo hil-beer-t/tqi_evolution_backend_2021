0ef6fecaa35e5154490eefb7e7845d988f2f79d9
Hilbert Digenio <hil.beer.t.git@gmail.com>
2022-01-10 10:26:47 -0300

    first commit, initial structure

---
+++ src/test/java/com/company/loansimulator/LoanSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.loansimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
